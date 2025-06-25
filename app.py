d0a63d67171a6eb1fc923f33506cdcaf23d92403
Marioalejop <marioalejop@gmail.com>
2025-06-24 19:51:04 -0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 def home():
     return render_template('index.html')
+
+@app.route('/datos')
+def datos_json():
+    return jsonify({"nombre" : "Mario", "edad": 30})
 
 if __name__ == '__main__':
     app.run(debug = True)
